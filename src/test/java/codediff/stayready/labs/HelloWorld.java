a4b8cfc29f7c3773b584b910ab972b1ac7bf4e60
Latoya4477 <87038766+Latoya4477@users.noreply.github.com>
2021-07-07 01:19:43 -0400

    Initial commit

---
+++ src/test/java/codediff/stayready/labs/HelloWorld.java
@@ -0,0 +1,15 @@
+package codediff.stayready.labs;
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("""
+                Name: Latoya Brown\s
+                Hometown: Newark DE
+                Major: Software Engineer\s
+                Favorite Song: Miss Independent by Ne-Yo
+                Favorite Superhero: Super Girl\s
+                Favorite Caffeine Source: I don’t do caffeine sorry.\s
+                """);
+    }
+}
+
